1f23100cd939ffc13a3ed17aa7ae219dc509a64e
kd1380248@kobedenshi078.onmicrosoft.com <kd1380248@kobedenshi078.onmicrosoft.com>
2026-05-14 19:29:25 +0900

    いろいろ完成系に近づけた

--- Program.cs
+++ Program.cs
@@ -11,15 +11,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
